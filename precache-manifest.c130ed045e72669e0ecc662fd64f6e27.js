10b2403f54067b17663127d627ca4e4988202e77
Nagar <prinagar@deloitte.com>
2019-08-12 12:50:13 +0530

    Updates

---
+++ precache-manifest.c130ed045e72669e0ecc662fd64f6e27.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e2e25b9fecbc9b08986ba6b43265d6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "623fb5e0a8c30bfac8b5",
+    "url": "/portfolio/static/css/main.e5b2dc51.chunk.css"
+  },
+  {
+    "revision": "decf1ce3948d7d4aff0f",
+    "url": "/portfolio/static/js/2.8d248648.chunk.js"
+  },
+  {
+    "revision": "623fb5e0a8c30bfac8b5",
+    "url": "/portfolio/static/js/main.36a26783.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "9a577249e013a431c982f80be8aaa384",
+    "url": "/portfolio/static/media/profile2.9a577249.jpg"
+  },
+  {
+    "revision": "bf56edc6c7090e92662e37a9b9ed2aea",
+    "url": "/portfolio/static/media/project1.bf56edc6.png"
+  },
+  {
+    "revision": "f7e79c7d61c8e4a45847337bcec527e6",
+    "url": "/portfolio/static/media/project2.f7e79c7d.png"
+  },
+  {
+    "revision": "7bded960f9e6263b998eb4529af6e041",
+    "url": "/portfolio/static/media/project3.7bded960.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  }
+]);
